250cada29ec13c3f41288b5b6b777577de1df74b
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-10 17:13:58 +0400

    created a class ,constructor and functions for age increase,display and relocate

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+class MasGroupAndCompanies():
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    year=2023#global variable
 
+    def __int__(self,name,age,place):#constructor
+        self.Name=name
+        self.Age=age
+        self.Place=place
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    def add_age(self):
+        self.Age=self.Age+1
 
+    def relocate(self,place):
+        self.Place=place
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    def display(self):
+        print(f"age is : {self.Age}  and place is :{self.Place}")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
