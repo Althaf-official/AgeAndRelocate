fa6db135500892c5bcc4cc65f798a25e3b722ac2
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-11 11:17:57 +0400

    created a class ,constructor and functions for age increase,display and relocate

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-class MasGroupAndCompanies():
-
-    year=2023#global variable
-
-    def __int__(self,name,age,place):#constructor
-        self.Name=name
-        self.Age=age
-        self.Place=place
+class Person:
+    def __init__(self, name, age, place):
+        self.name = name
+        self.age = age
+        self.place=place
 
     def add_age(self):
-        self.Age=self.Age+1
+        self.age=self.age+1
+
+    def introduce(self):
+        print(f"My name is {self.name} and I am {self.age} years old. and my place is {self.place}")
 
     def relocate(self,place):
-        self.Place=place
+        self.place=place
 
-    def display(self):
-        print(f"age is : {self.Age}  and place is :{self.Place}")
+# Creating an instance of the Person class with arguments
+person1 = Person("Muhammed althaf", 27, "kallambalam")
+person1.introduce()  # Output: My name is Alice and I am 25 years old.
+
+person2 = Person("jaseela ashraf ", 21, "idavoorkonam")
+person2.relocate("madanthapacha")
+person2.add_age()
+person2.introduce()  # Output: My name is Bob and I am 30 years old.
+
+
+
+
 
