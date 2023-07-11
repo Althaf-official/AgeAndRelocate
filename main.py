c97e976573a9a71e8aafdaafd4d888214de0082b
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-07-11 11:38:21 +0400

    Class method explained

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class Person:
+    year=2023
     def __init__(self, name, age, place):
         self.name = name
         self.age = age
@@ -7,21 +8,52 @@ class Person:
     def add_age(self):
         self.age=self.age+1
 
-    def introduce(self):
-        print(f"My name is {self.name} and I am {self.age} years old. and my place is {self.place}")
-
     def relocate(self,place):
         self.place=place
 
+    def display(self):
+        print(f"The year is {str(Person.year)} and My name is {self.name} and I am {self.age} years old. and my place is {self.place}")
+
+    #class method function
+    """
+classmethod decorator is used to define a class method. 
+A class method is a method that belongs to the class itself rather than an instance of the class. 
+It can be called on the class directly, without the need to create an instance of the class.
+To define a class method, you need to decorate a method within the class using the @classmethod decorator. 
+The method should have a special first parameter conventionally named cls, which refers to the class itself. 
+This parameter allows you to access and modify class-level variables and call other class methods.
+    """
+    @classmethod
+    def add_year(cls):
+        Person.year=Person.year+1
+
 # Creating an instance of the Person class with arguments
 person1 = Person("Muhammed althaf", 27, "kallambalam")
-person1.introduce()  # Output: My name is Alice and I am 25 years old.
-
 person2 = Person("jaseela ashraf ", 21, "idavoorkonam")
-person2.relocate("madanthapacha")
-person2.add_age()
-person2.introduce()  # Output: My name is Bob and I am 30 years old.
 
+person1.display()
+person2.display()
+
+print("________________________________________________")
+
+Person.year=Person.year+1
+person1.add_age()
+person2.add_age()
+
+person1.display()
+person2.display()
+
+print("________________________________________________")
+Person.add_year()
+person1.add_age()
+person2.add_age()
+person1.relocate("Abu Dhabi")
+person2.relocate("madanthapacha")
+
+person1.display()
+person2.display()
+
+print("________________________________________________")
 
 
 
